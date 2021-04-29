e68c28f9389f929ac3edafbace9541ebc55f5cfc
yuanyunxiao <yuanyunx@126.com>
2021-04-30 02:02:29 +0800

    add config

--- src/main/java/com/novel/recommen/model/NovelInfo.java
+++ src/main/java/com/novel/recommen/model/NovelInfo.java
@@ -8,7 +8,7 @@ public class NovelInfo {
     private String novelName; //小说名字
     private int authorId;  //作者名字
     private String state;  //小说状态：连载/完结
-    private String novelIntroduction;  //小说简介
+    private String introduce;  //小说简介
     private String coverUrl;  //小说封面
     private String tag;  //小说标签
 }
