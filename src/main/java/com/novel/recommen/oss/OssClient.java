0bf21cb05cdac14779cdb3b18911719da5e68573
yuanyunxiao <yuanyunx@126.com>
2021-05-19 17:24:41 +0800

    v13

--- src/main/java/com/novel/recommen/oss/OssClient.java
+++ src/main/java/com/novel/recommen/oss/OssClient.java
@@ -5,6 +5,7 @@ import com.aliyun.oss.model.OSSObject;
 import com.aliyun.oss.model.PutObjectRequest;
 import com.aliyun.oss.model.PutObjectResult;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.tomcat.jni.Time;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
@@ -13,19 +14,28 @@ import java.io.InputStreamReader;
 
 public class OssClient {
     public static OSS client = OSSClientBuilder.GetClient();;
-    public static String GetRandomBook() throws IOException {
+    public static String GetRandomBook() {
         String bookContent = "";
         int n = (int)(Math.random()*34);
-        OSSObject ossObject = client.getObject(OSSClientBuilder.bucketName, "book"+n+".txt");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(ossObject.getObjectContent()));
-        while (true) {
-            String line = reader.readLine();
-            if (line == null) break;
-            bookContent += line;
-            System.out.println(line);
-        }
-        if (StringUtils.isBlank(bookContent)){
-
+        while (true){
+            try{
+                bookContent = "";
+                OSSObject ossObject = client.getObject(OSSClientBuilder.bucketName, "book"+n+".txt");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(ossObject.getObjectContent()));
+                while (true) {
+                    String line = reader.readLine();
+                    if (line == null) break;
+                    bookContent += line;
+                    System.out.println(line);
+                }
+                break;
+            }catch (Exception e){
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+            }
         }
         return bookContent;
     }
